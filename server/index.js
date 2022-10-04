d0c757afd2c5d0fb246614470d2b18d260149731
Ty Menlove <tjmundecided@gmail.com>
2022-10-04 16:55:42 -0600

    trying to fix hosting

--- server/index.js
+++ server/index.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const cors = require('cors')
+const path = require('path')
+
+require('dotenv').config()
+
 const port = process.env.PORT || 5000
 const app = express()
 
@@ -13,6 +17,13 @@ const {
     deleteResume,
     addResume
 } = require('./controller')
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..client/index.html'))
+})
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, '..client/styles.css'))
+// })
 
 app.get('/getResume', getResume)
 app.delete('/deleteResume/:id', deleteResume)
